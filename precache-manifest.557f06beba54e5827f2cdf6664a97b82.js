f5cefc64bbf587fa9979abfd248ad30ff2b534ec
xand <xandjiji@gmail.com>
2019-07-24 23:49:24 -0300

    Updates

---
+++ precache-manifest.557f06beba54e5827f2cdf6664a97b82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6de5547f5ac943c26235a76812684e",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "aaf7c3ccfe1e97323249",
+    "url": "/personal-website/static/css/main.c5d587af.chunk.css"
+  },
+  {
+    "revision": "30d772f406668a7544c4",
+    "url": "/personal-website/static/js/2.6296308c.chunk.js"
+  },
+  {
+    "revision": "aaf7c3ccfe1e97323249",
+    "url": "/personal-website/static/js/main.bebf2f74.chunk.js"
+  },
+  {
+    "revision": "b6d8e3a721968ec40bad",
+    "url": "/personal-website/static/js/runtime~main.93f2c6c9.js"
+  }
+]);
