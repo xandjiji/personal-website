6328db686d5a432d0f34dee78d3df236d5612dc3
xand <xandjiji@gmail.com>
2019-07-26 09:25:12 -0300

    clicking actions

--- src/components/ContainerItem.js
+++ src/components/ContainerItem.js
@@ -3,45 +3,27 @@ import '../css/ContainerItem.css';
 
 export class ContainerItem extends Component {
 
-    componentDidMount() {
-        window.addEventListener('wheel', this.handleScroll);
-    }
-    
-    componentWillUnmount() {
-        window.removeEventListener('wheel', this.handleScroll);
+    expand = (id) => {
+        alert("my id is: " + id);
     }
 
-    /* handleScroll(event) {
-        if(event.deltaY > 0) {
-            let newIndex = this.state.index;
-            newIndex++;
-
-            if(newIndex < ((this.state.items.length / 2) - 1)) {
-                this.setState({index: newIndex})
-            }
-        } else {
-            let newIndex = this.state.index;
-            newIndex--;
-
-            if(newIndex >= 0){
-                this.setState({index: newIndex})
-            }
-        }
-    } */
-
-    /* handleScroll(event) {
-        if(event.deltaY > 0) {
-            this.setState({index: 1})
-        } else {
-            
-        }
-    } */
+    /* 
+    atributo a mais no estado ([0] = normal / [1-9] = expandido)
+    salvar o estado anterior
+    mudar o estado atual
+    fazer um if para renderizar apenas o item expandido do estado
+    inline style para ele ocupar toda a tela
+    botao de voltar para o estado anterior
+    */
 
     render() {
         return this.props.state.items.map((item) => (
-            <div key={item.id} className="Item" style={{
-                'transform': `translateY(-${this.props.state.index*(100)}%)`
-            }}>
+            <div    key={item.id}
+                    ref={item.id}
+                    className="Item"
+                    style={{'transform': `translateY(-${this.props.state.index*(100)}%)`}}
+                    onClick={() => this.expand(item.id)}>
+
                 <div className="Box">
                     <h2>{item.title}</h2>
                     <div className="Contents">
